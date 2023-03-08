dee7837515575017de2021c40eb6d63affb34922
George Hyland <gohylander@gmail.com>
2023-03-07 20:01:10 -0600

    arith lab

---
+++ index.js
@@ -0,0 +1,68 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add() {
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract() {
+    return "something";
+}
+
+function subtract(a, b) {
+    return "something";
+}
+function subtract(a, b) {
+    return a - b;
+}
+function mutiply() {
+    return "something"; 
+}
+function multiply(a, b) {
+    return "something"
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide() {
+    return "something";
+}
+function divide(a, b) {
+    return "something"
+}
+function divide(a, b) {
+    return a / b;
+}
+let n = 0;
+function increment() {
+
+}
+function increment(n) {
+    return (n += 1);
+}  
+function decrement() {
+
+}
+function decrement(n) {
+    return (n -= 1)
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n); 
+}
